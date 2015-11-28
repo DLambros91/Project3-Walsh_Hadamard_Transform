ac71f70e7925952d21377ca14def3ab5dce5dcbe
Harsh Shah <hms96@scarletmail.rutgers.edu>
2015-11-27 23:56:24 -0800

    Generating the Hadamard Matrix
    Trying to use calculation method yyz gave, doesnt work yet

--- serial/WHT.c
+++ serial/WHT.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <limits.h>
 
 int main(int argc, char** argv){
 	
@@ -20,29 +20,67 @@ int main(int argc, char** argv){
 		m = atoi(argv[1]);
 	}
 
+	int dimension; //Holds the Row and Column size of matrix
+	dimension = (1 << m); // 2^m
+
+	//Display Input to user
+	printf("Input M is: %d ", m);
+	printf("Row and Column size is %d", dimension);
+
 	//Allocate Vector
-	int *vector = (int*)malloc((1 << m) * sizeof(int));
+	int *vector = (int*)malloc(dimension * sizeof(int));
 		
 	//Allocate Matrix
 	int ** matrix;
-	matrix = (int**)malloc(sizeof(int**)* (1 << m));
+	matrix = (int**)malloc(sizeof(int**)* dimension);
 	int z = 0;
-	for(; z < (1 << m); z++){
-		matrix[z] = (int*)malloc(sizeof(int)*(1 << m));
+	for(; z < dimension; z++){
+		matrix[z] = (int*)malloc(sizeof(int)*dimension);
 	}
 
-	printf("Input M is: %d ", m);
-	printf("Row and column size is %d", (1 << m));
+	//Allocate Result Vector
+	int* result = (int*)malloc(sizeof(int) * dimension);
 
+	//Fill vector with random integers
+	z = 0;
+	for(; z < dimension; z++){
+		vector[z] = rand()%2;
+	}
+
+
+	//Generate the Hadamard Matrix
+	int i,j;
+	for(i = 0; i < dimension; i++){
+		for(j = 0; j < dimension; j++){ //iterate through matrix
+			unsigned int PosOrNeg = (i*j) && (UINT_MAX); //AND the Product of the location indices with a string one '1''s 
+			int ones = __builtin_popcount(PosOrNeg); //Count the number of ones in PosOrNeg to decide if index should be 1 or -1
+			if((ones % 2) == 0){
+				matrix[i][j] = 1;
+			}else{
+				matrix[i][j] = -1;
+			}
+		}
+	}
+
+
+
+	//Print the Matrix to Test for Correctness
+	for(i = 0; i < dimension; i++){
+		for(j = 0; j < dimension; j++){
+			printf("%d", matrix[i][j]);
+		}
+		printf("\n");
+	}
 
 	//Free the memory
 	free(vector);
 	z = 0;
-	for(;z < (1 << m); z++){
+	for(;z < dimension; z++){
 		free(matrix[z]);
 	}
 	free(matrix);
-	
+	free(result);
+
 	return 0;
 
 }
